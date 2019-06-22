3f49cf559c8355a7de53177fa786ae30fa4936db
NickDaFox <Nick.Banken@gmail.com>
2019-06-22 16:24:28 +0200

    added new homepage

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,3 +2,6 @@ $(document).on('click', '[data-toggle="lightbox"]', function(event) {
                 event.preventDefault();
                 $(this).ekkoLightbox();
             });
+
+
+            
