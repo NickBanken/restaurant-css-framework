3a941e8adbed0f53c39083c6fe8ee0d22faaac8a
NickDaFox <Nick.Banken@gmail.com>
2019-06-23 23:20:14 +0200

    finished the layout and content of home page, need to add the translate function

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,29 @@
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
-                event.preventDefault();
-                $(this).ekkoLightbox();
-            });
+	event.preventDefault();
+	$(this).ekkoLightbox();
+});
 
+var dataReload = document.querySelectorAll('[data-reload]');
 
-            
+var language = {
+	eng: {
+		deliverytime: "Most delicious pizza's &lt;br /> Hot delivery within 30mins!";
+	},
+	fr: {
+		deliverytime: 'GetRekt'
+	}
+};
+
+if (window.location.hash) {
+	if (window.location.hash === '#fr') {
+		deliverytime.textContent = language.fr.deliverytime;
+	} else {
+		deliverytime.textContent = language.eng.deliverytime;
+	}
+}
+
+for (i = 0; i <= dataReload.length; i++) {
+	dataReload[i].onclick = function() {
+		location.reload(true);
+	};
+}
